5d531e4b98cbc0f8b69d36f9c9bce5ca59859f52
Aras Balali Moghaddam <arasbm@gmail.com>
2013-10-20 22:23:46 -0700

    basic testing of touch events

--- src/main.js
+++ src/main.js
@@ -1,27 +1,63 @@
 (function() {
-  var template = '<div class="tough-switch-wrapper">' +
-    'tough switch test' +
-    '<div id="handle"></div>' +
-    '</div>';
+  var handle;
+  var container;
 
+  function setupTouchEvents() {
+    xtag.addEvent(container, 'pinch', function(e) {
+      console.log('pinch ' + e.detail.direction.toUpperCase());
+      e.baseEvent.preventDefault();
+      handle.style.top = e.detail.centroid.y + 'px';
+      handle.style.left = e.detail.centroid.x + 'px';
+    });
+
+    xtag.addEvent(container, 'touchstart', function(e) {
+      e.preventDefault();
+      console.log('touchstart  ', e);
+      handle.style.top = e.detail.centroid.y + 'px';
+      handle.style.left = e.detail.centroid.x + 'px';
+
+    });
+
+    xtag.addEvent(container, 'swipe', function(e) {
+      e.preventDefault();
+      console.log('swipe', e);
+      handle.style.top = e.detail.centroid.y + 'px';
+      handle.style.left = e.detail.centroid.x + 'px';
+    });
+
+    xtag.addEvent(container, 'longtap', function(e) {
+      e.preventDefault();
+      console.log('long tap', e);
+      handle.style.top = e.detail.centroid.y + 'px';
+      handle.style.left = e.detail.centroid.x + 'px';
+    });
+  }
   xtag.register('x-tough-switch', {
     lifecycle: {
       created: function() {
         this.setAttribute('tabindex', this.getAttribute('tabindex') || 0);
-        this.innerHTML = template;
+        container = document.createElement('div');
+        handle = document.createElement('div');
+        handle.classList.add('handle');
+        container.classList.add('container');
+        this.appendChild(handle);
+        this.appendChild(container);
         this.onText = this.onText;
         this.offText = this.offText;
         this.checked = this.checked;
         this.onIcon = this.onIcon;
         this.offIcon = this.offIcon;
+        setupTouchEvents();
       },
       inserted: function() {},
       removed: function() {},
       attributeChanged: function() {}
     },
     events: {
-     'tap:preventable:delegate(div)': function() {
-        alert('tapped on tough switch');
+     'tap:preventable:delegate(div)': function(e) {
+        handle.style.top = e.clientY + 'px';
+        handle.style.left = e.clientX + 'px';
+        console.log('tapped', e);
       }
     },
     accessors: {
